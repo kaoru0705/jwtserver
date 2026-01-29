a69333a682dca4dd908e03fd5c4d0ad324b38547
kaoru0705 <51065756+kaoru0705@users.noreply.github.com>
2026-01-29 15:03:44 +0900

    jwtToken

--- src/main/java/com/ch/jwtserver/member/controller/MemberController.java
+++ src/main/java/com/ch/jwtserver/member/controller/MemberController.java
@@ -1,11 +1,15 @@
 package com.ch.jwtserver.member.controller;
 
+import com.ch.jwtserver.member.dto.LoginResponse;
 import com.ch.jwtserver.member.dto.MemberRequest;
 import com.ch.jwtserver.member.dto.MemberResponse;
 import com.ch.jwtserver.member.entity.Member;
+import com.ch.jwtserver.member.jwt.JwtTokenProvider;
 import com.ch.jwtserver.member.service.AuthService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -13,9 +17,11 @@ import org.springframework.web.bind.annotation.*;
 public class MemberController {
 
     private final AuthService authService;
+    private final JwtTokenProvider jwtTokenProvider;
 
-    public MemberController(AuthService authService) {
+    public MemberController(AuthService authService, JwtTokenProvider jwtTokenProvider) {
         this.authService = authService;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     @GetMapping("/test")
@@ -24,7 +30,7 @@ public class MemberController {
     }
 
     @PostMapping("/login")
-    public MemberResponse login(@RequestBody MemberRequest memberRequest) throws IllegalAccessException{
+    public LoginResponse login(@RequestBody MemberRequest memberRequest) throws IllegalAccessException{
         // 원래는 스프링 시큐리티가 알아서 회원정보 검증을 수행하지만, 현재로는 formLogin 비활성화 시켜놓음
         // 참고로 formLogin.disable()했다고 해서, 시큐리티의 필터체인이 동작하지 않는 건 아님
         // formLogin.disable() 은 UsernamePasswordAuthenticationFilter를 통한 요청 흐름을 안 타겠다는 의미
@@ -42,9 +48,15 @@ public class MemberController {
         }
 
         log.debug("회원 인증 성공");
-        
-        // 비밀번호 검증을 처리..
-        return null;
+
+        // 로그인에 성공한 유저에게는 응답 정보에 JWT 토큰을 적재하여 보내주자
+        //jwtTokenProvider.createAccessToken(회원 아이디, 권한);
+        String token = jwtTokenProvider.createAccessToken(member.getHomepageId(), List.of("ROLE_USER"));
+
+        log.debug("token is {}", token);
+
+        // Bearer Http 통신 시 헤더에 넣을 수 있는 공식적인 헤더값(인증 토큰 전송 시 그 형식을 의미)
+        return new LoginResponse(token, "Bearer");
     }
 
     // 연습용 회원의 암호 생성 요청을 처리
---
+++ src/main/java/com/ch/jwtserver/member/jwt/JwtTokenProvider.java
@@ -0,0 +1,61 @@
+package com.ch.jwtserver.member.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+import java.util.Date;
+import java.util.List;
+
+/*
+    JWT 토큰을 발급하고, 해석을 담당하는 클래스
+    jjwt api        0.12.6
+    jjwt impl       0.12.6
+    jjwt jackson    0.12.6
+ */
+@Component
+public class JwtTokenProvider {
+
+    private SecretKey signature;
+
+    public JwtTokenProvider(@Value("${app.jwt.secret}") String secret) {
+        this.signature = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
+
+    /*---------------------------------------------------------
+    토큰 생성
+    ---------------------------------------------------------*/
+    public String createAccessToken(String user, List<String> roles) {
+        Instant now = Instant.now();
+        // 실무적으로 5분에서 10분
+        Instant exp = now.plusSeconds(60 * 60);
+
+        // new JWT(A, B, C, D)
+        return Jwts.builder()
+                .subject(user)    // sub 주제, 무엇과 관련된 것인가? 회원과 관련된 것임
+                //.claim("key", "value")  // 공식 표준 데이터는 아니지만 개발자들이 주로 권한..(일반 유저, 관리자...)
+                .claim("roles", "ROLE_USER")
+                .issuedAt(Date.from(now))
+                .expiration(Date.from(exp))
+                .signWith(signature)     // 서명
+
+                .compact();    //
+    }
+
+    /*---------------------------------------------------------
+    토큰 해석 ( 토큰 안의 값 추출)
+    ---------------------------------------------------------*/
+    public Claims parseClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(signature)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+}
